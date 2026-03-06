5dd898ffa6991a3d5056bed3809bdb5b61a9ffb2
Robin Müller <mueller@fsr.tu-darmstadt.de>
2026-03-06 14:51:29 +0100

    Revert "Trimmed the branch to only the auto_apms_mission sub directory"
    This reverts commit 65699acf527a0a878481062a1f3f897d5b103df8.

---
+++ auto_apms_util/include/auto_apms_util/logging.hpp
@@ -0,0 +1,41 @@
+// Copyright 2024 Robin Müller
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "rclcpp/logger.hpp"
+
+namespace auto_apms_util
+{
+
+/// @ingroup auto_apms_util
+/// @{
+
+/**
+ * @brief Set the logging severity of a ROS 2 logger.
+ * @param logger Logger instance.
+ * @param severity Desired severity level encoded as a string. Must be one of DEBUG, INFO, WARN, ERROR, FATAL or UNSET.
+ */
+void setLoggingSeverity(const rclcpp::Logger & logger, const std::string & severity);
+
+/**
+ * @brief Set the logging severity of a ROS 2 logger.
+ * @param logger Logger instance.
+ * @param severity Desired severity level.
+ */
+void setLoggingSeverity(const rclcpp::Logger & logger, rclcpp::Logger::Level severity);
+
+/// @}
+
+}  // namespace auto_apms_util
