9c102cb528d173ef660c66463fa87b67048786ad
Robin Müller <mueller@fsr.tu-darmstadt.de>
2026-03-06 14:58:10 +0100

    Rebase from 'upstream'

---
+++ auto_apms_behavior_tree_core/include/auto_apms_behavior_tree_core/node/node_registration_from_library_adapter.hpp
@@ -0,0 +1,89 @@
+// Copyright 2026 Robin Müller
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "auto_apms_behavior_tree_core/node/node_registration_interface.hpp"
+
+namespace auto_apms_behavior_tree::core
+{
+
+/**
+ * @brief A node registration interface that is able to load and registers behavior tree nodes from a shared library
+ * using `BT::BehaviorTreeFactory::registerFromPlugin`.
+ *
+ * The libraries must export the `BT_RegisterNodesFromPlugin(factory)` symbol which is the ROS-agnostic approach for
+ * distributing behavior tree nodes through plugins in BehaviorTree.CPP.
+ *
+ * You can find an example of how to implement the required plugin export macro in the official BehaviorTree.CPP
+ * repository:
+ * https://github.com/BehaviorTree/BehaviorTree.CPP/blob/3ff6a32ba0497a08519c77a1436e3b81eff1bcd6/examples/plugin_example/plugin_action.cpp
+ *
+ * **Usage:**
+ *
+ * 1. Create a subclass of this adapter for each plugin library you want to load nodes from and implement the
+ *    NodeRegistrationFromLibraryAdapter::getPluginLibraryPath() method to return the correct library path.
+ *
+ * 2. Use the NODE_REGISTRATION_TYPE keyword in the `auto_apms_behavior_tree_register_nodes` macro in your
+ *    CMakeLists.txt to specify the adapter subclass as the registration type for the nodes you want to register from
+ *    the plugin library.
+ *
+ *    ```cmake
+ *    auto_apms_behavior_tree_register_nodes(my_nodes
+ *      my_package::MyNodeRegistrationType
+ *      NODE_REGISTRATION_TYPE my_package::MyNodeRegistrationType
+ *    )
+ *    ```
+ *
+ *    In this case, you need to specify `my_package::MyNodeRegistrationType` both under ARGN and NODE_REGISTRATION_TYPE.
+ *    Supplying multiple args under ARGN would create ambiguity, because there would be multiple names for the same
+ *    collection of nodes. The convention is to call this macro once for each node registration type (if it's not a
+ *    template).
+ *
+ * 3. In your node manifest YAML, specify the fully qualified name of the adapter subclass as the class name for the
+ *    nodes you want to register from the plugin library. You can specify the same adapter subclass multiple times with
+ *    different registration names to register multiple nodes from the same library.
+ *
+ *    ```yaml
+ *    MyNodeName:
+ *      class_name: my_package::MyNodeRegistrationType
+ *
+ *     MyOtherNodeName:
+ *       class_name: my_package::MyNodeRegistrationType
+ *    ```
+ *
+ *    As you see, it is also possible to register multiple nodes from the same plugin library using the same adapter
+ *    subclass.
+ */
+class NodeRegistrationFromLibraryAdapter : public NodeRegistrationInterface
+{
+public:
+  NodeRegistrationFromLibraryAdapter() = default;
+  ~NodeRegistrationFromLibraryAdapter() override = default;
+
+  bool requiresRosNodeContext() const override;
+
+  void registerWithBehaviorTreeFactory(
+    BT::BehaviorTreeFactory & factory, const std::string & name,
+    const auto_apms_behavior_tree::core::RosNodeContext * const context_ptr = nullptr) const override;
+
+private:
+  /**
+   * @brief Get the library path that exports the node registration function associated with this class.
+   * @return Library path.
+   */
+  virtual std::string getPluginLibraryPath() const = 0;
+};
+
+}  // namespace auto_apms_behavior_tree::core
